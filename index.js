dfd6bb9858c5af3de3568884062a35dc5632eb9b
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-08-04 22:59:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+ function writeCards(names){
+  var cardMessages = []
+  for (let i = 0; i < names.length; i++){
+    var messages = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    cardMessages.push(messages);
+
+    debugger;
+  }
+  return cardMessages;
+ }
+ writeCards(names);
+
+function countDown(number) {
+  let x = number;
+  while (x >= 0) {
+    console.log(x);
+    x--;
+  }
+}
